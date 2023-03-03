10f941fac95355318f04f679d721b420bbb927d8
Jerakin <the_rensvind@hotmail.com>
2023-03-03 01:16:35 +0100

    3d model example

---
+++ assets/3d_logo/defold.go
@@ -0,0 +1,38 @@
+components {
+  id: "defold_logo"
+  component: "/assets/3d_logo/defold_logo.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/assets/3d_logo/Defold.glb\"\n"
+  "material: \"/builtins/materials/model.material\"\n"
+  "textures: \"/assets/3d_logo/defold.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
